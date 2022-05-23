5b3735eef6a33c73b768013c898969dd92fde5ca
Ezequias Evangelista <ezequias_santos_sp@yahoo.com.br>
2022-05-23 12:59:35 -0300

    adicionando um console.log

--- app.js
+++ app.js
@@ -1,2 +1,4 @@
 console.log("Rodando o sitema de gerenciamento de jogos")
 console.log("aplicação está se conectando ao banco de dados")
+console.log("o aspira fez esse commit sem vc ver na aula! ")
+console.log("Enviando para o GitHub")
